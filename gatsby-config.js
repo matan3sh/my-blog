a91a01e7aec41f5e0b702762d01f412384dc7d5e
Matan Shaviro <matan3sh@gmail.com>
2021-05-14 23:29:14 +0300

    Gatsby basics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,18 @@
+const path = require('path');
+
 module.exports = {
   siteMetadata: {
-    title: "my-blog",
+    title: 'my-blog',
   },
-  plugins: ["gatsby-plugin-sass"],
+  plugins: [
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-plugin-root-import',
+      options: {
+        src: path.join(__dirname, 'src'),
+        pages: path.join(__dirname, 'src/pages'),
+        components: path.join(__dirname, 'src/components'),
+      },
+    },
+  ],
 };
